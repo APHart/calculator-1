d641e0a95298018c6f39fbf7ac06289b8d2ceaf6
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-04 12:40:25 -0700

    Added add_mult function.

--- arithmetic.py
+++ arithmetic.py
@@ -47,3 +47,8 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Returns the product of the sum of num1 and num2 multiplied by num3."""
+
+    return (num1 + num2) * num3
